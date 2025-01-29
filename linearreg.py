53fccc8d78eadfbbc5ab8362ed0b0c6b8289d2cd
Filipe Esteves <filipecastroesteves@gmail.com>
2025-01-29 16:33:37 +0000

    Update linearreg.py

--- linearreg.py
+++ linearreg.py
@@ -12,25 +12,28 @@ def minimos_quadrados(x, y):
 
 
 def main():
-    data = pnd.read_excel
-    solea = data.loc[0:, ['solea']]
-    mud = data.loc[0:, ['mud']]
+    data = pnd.read_excel('Localização do ficheiro')
     solea = data['solea'].values
     mud = data['mud'].values
-    m = minimosquadrados(mud_list, solea_list)
+    
+    m = minimosquadrados(mud, solea)
     media_solea = sum(solea) / len(solea)
     media_mud = sum(mud) / len(mud)
     b = media_solea - m * media_mud
     r = lambda x: m * x + b
     data_X = [1, 100]
     data_Y = [r(data_X[0]), r(data_X[1])]
+    
     plt.figure(figsize=(8, 6), dpi=(300))
     plt.plot(mud, solea, 'ko')
-    plt.plot(data_X, data_Y, 'r-')
+    plt.plot(data_X, data_Y, 'r --', label = 'y = -0.0151x + 1.1549')
     plt.axis([0, 100, -0.1, 1.1])
     plt.ylabel('Presença / Ausência de Solha')
     plt.xlabel('Percentagem de Argila(%)')
+    plt.legend(loc= 'upper center', bbox_to_anchor= (0.5, -0.05), 
+               fancybox = True, shadow = True, ncol = 5)
     plt.show()
+    
     print('Eq. da Regressão Linear:')
     print(f'y = {m}x + {b}\n' )
     print('Eq. da Reg.Linear(arredondado a 3 casas décimais):')
